c0579b49398786b64f62fdae9d64974873f0403f
kyoung90 <kenneth.young90@gmail.com>
2019-06-27 18:24:24 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+class Directors extends Component {
+
+  generateDirectors(){
+    return (
+      directors.map(director => {
+        return (
+          <div>
+            <h2>Title: {director.name}</h2>
+            <h3>Movies</h3>
+            <ul>
+              {director.movies.map(movie => {
+                return <li>{movie}</li>
+              })}
+            </ul>
+          </div>
+        )
+      })
+    )
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Directors Page</h1>
+        {this.generateDirectors()}
+      </div>
+    );
+  }
+  
 }
 
 export default Directors
