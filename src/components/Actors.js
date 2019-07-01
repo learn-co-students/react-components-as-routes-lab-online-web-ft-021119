bf1fa30eebc14e5b5acfbbc794f4f932beec2c25
VBTX <jv.svedaite@gmail.com>
2019-07-01 11:57:12 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,27 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+      	<div key={index} className="actor">
+      	<h3>Name: {actor.name} </h3>
+      	<h3>Movies:</h3>
+      	<ul>
+      	{actor.movies.map((movie, index) => (
+      		<li key={index}>{movie}</li>
+
+      		))}
+      	</ul>
+      	</div>
+      	))}
     </div>
   );
 };
 
+
 export default Actors;
+
+
+
+
+    
