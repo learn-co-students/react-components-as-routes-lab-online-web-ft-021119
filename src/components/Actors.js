48f3f5e36a5fb46a99271ea2a3aaf247349939fb
github username <github email address>
2019-07-02 14:05:02 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,51 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+      <h1>Actors Page</h1>
+       {actors.map((actor, index) => (
+       	<div key={index} className="actor">
+         	<hr></hr><h3>{actor.name}</h3>
+         	<h4>Movies:</h4>
+         	<ul>
+           	{actor.movies.map((movie, index) => (
+           		<li key={index}>{movie}</li>
+           		))}
+         	</ul>
+       	</div>
+       	))}
+     </div>
   );
 };
 
 export default Actors;
+
+
+
+// {actors.map((actor, index) => (
+//        <div key={index} className="actor">
+//        <h3>Name: {actor.name} </h3>
+//        <h3>Movies:</h3>
+//        <ul>
+//        {actor.movies.map((movie, index) => (
+//          <li key={index}>{movie}</li>
+//
+//          ))}
+//        </ul>
+
+// <div className="actor">
+//   {actors.map(actor => actor.name)}
+//     {actor.movies.map(movie => {
+//       <ul>{movie}</ul>
+//     })}
+// </div>
+// </div>
+// debugger
+// {actors.map(actor => actor.movies.map(movie => {
+//   <ul>{movie}</ul>
+//   }))}
+// </div>
+// </div>
+//
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-}
+  return(
+  <div>
+    <h1>Directors Page</h1>
+     {directors.map((director, index) => (
+      <div key={index} className="director">
+        <hr></hr><h3>{director.name}</h3>
+        <h4>Movies:</h4>
+        <ul>
+          {director.movies.map((movie, index) => (
+            <li key={index}>{movie}</li>
+            ))}
+        </ul>
+      </div>
+      ))}
+   </div>
+);
+};
 
 export default Directors
