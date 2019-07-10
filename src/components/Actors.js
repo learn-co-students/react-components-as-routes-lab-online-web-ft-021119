0551b7470d2536857c92bdfa2e57468b3330ae44
kirstenwerner <kirstenmwerner@gmail.com>
2019-07-10 00:17:36 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { actors } from '../data';
 
+const renderActors = (actors) => {
+  return (
+    actors.map(function(actor) {
+      return (
+        <div>
+          <h4>Name: { actor.name }</h4>
+          <p>Movies:
+            <ul>
+              {actor.movies.map(movie => <li>{movie}</li>)}
+            </ul>
+          </p>
+        </div>
+      )
+    })
+  )
+}
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors(actors)}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderMovies = (movies) => {
+  return (
+    movies.map(function(movie) {
+      return (
+        <div>
+          <h4>Name: { movie.title }</h4>
+          <p>Time: { movie.time }</p>
+          <p>Genres:
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </p>
+        </div>
+      )
+    })
+  )
+}
+
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies(movies)}
     </div>
   );
 };
